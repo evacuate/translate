abe83a3fd533da9a5f40699f201d3ace73bd6efe
minagishl <mvxd568.co@icloud.com>
2024-10-14 05:18:35 +0900

    Create other tests in reserve.

--- tests/index.ts
+++ tests/index.ts
@@ -12,3 +12,13 @@ test('translate 2', ({ expect }) => {
   const text = translate('Tokyo');
   expect(text).toBe('東京都');
 });
+
+test('translate 3', ({ expect }) => {
+  const text = translate('神奈川県');
+  expect(text).toBe('Kanagawa');
+});
+
+test('translate 4', ({ expect }) => {
+  const text = translate('Kanagawa');
+  expect(text).toBe('神奈川県');
+});
